5183f4f53c096586deaa2579b95176525e0f2d2b
judigot <judigot@github.com>
2023-03-30 16:43:49 +0800

    Fix: dropdown behavior, bug when adding back cc/bcc

--- imports/ui/Form.tsx
+++ imports/ui/Form.tsx
@@ -37,20 +37,20 @@ const Recipient = ({
   actions,
 }: Recipient) => {
   const [isFocused, setIsFocused] = React.useState<boolean>(false);
-  const usersSource = inputRef.current.value ? filteredUsers : allUsers;
+  const usersSource = inputRef.current?.value ? filteredUsers : allUsers;
   return (
     <div
       className="border-b"
       style={{
         display: "grid",
         gridTemplateColumns: "minmax(200px, 1fr) max-content",
+        position: "relative",
       }}
     >
       <div className="my-auto">
         <span>{label}</span>
         <span>
           {allUsers &&
-            allUsers.length !== 0 &&
             allUsers.map((row, i, array) => {
               if (selectedUsers.includes(row.id)) {
                 return (
@@ -86,34 +86,34 @@ const Recipient = ({
               style={{ width: "100%" }}
               className="border-transparent focus:border-transparent focus:ring-0 py-1.5 text-gray-900  ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6"
             />
-            {isFocused && usersSource && usersSource.length !== 0 && (
-              <ul
-                className="cursor-pointer bg-slate-800 text-white rounded-md p-3 h-min max-h-80 overflow-y-scroll w-80"
-                style={{ position: "absolute" }}
-              >
-                {usersSource.map((row: User, i: number, array: User[]) => {
-                  if (!selectedUsers.includes(row.id)) {
-                    return (
-                      <li
-                        key={row.id}
-                        className="p-1"
-                        onMouseDown={() => {
-                          onSelectUser(id, row.id, inputRef);
-                          setTimeout(() => {
-                            inputRef.current.focus();
-                          });
-                        }}
-                      >
-                        <div className="font-semibold">{row.name}</div>
-                        <div>{row.email}</div>
-                      </li>
-                    );
-                  }
-                })}
-              </ul>
-            )}
           </div>
         </span>
+        {isFocused &&
+          usersSource?.length !== 0 &&
+          // Hides dropdown when all users are selected
+          usersSource?.length !== selectedUsers.length && (
+            <ul className="z-10 absolute w-full cursor-pointer bg-slate-800 text-white rounded-md p-3 h-min max-h-80 overflow-y-scroll w-80">
+              {usersSource?.map((row: User, i: number, array: User[]) => {
+                if (!selectedUsers.includes(row.id)) {
+                  return (
+                    <li
+                      key={row.id}
+                      className="p-1"
+                      onMouseDown={() => {
+                        onSelectUser(id, row.id, inputRef);
+                        setTimeout(() => {
+                          inputRef.current.focus();
+                        });
+                      }}
+                    >
+                      <div className="font-semibold">{row.name}</div>
+                      <div>{row.email}</div>
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+          )}
       </div>
 
       <div className="inline-block" style={{ margin: "auto 0% auto 0%" }}>
