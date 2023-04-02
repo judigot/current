9a90086210f9c5a1392b584ebb9bded34b528221
judigot <judigot@github.com>
2023-04-02 17:00:09 +0800

    Refactor: remove unused code

--- imports/api/LinksMethods.ts
+++ imports/api/LinksMethods.ts
@@ -1,10 +1,6 @@
 import { Meteor } from "meteor/meteor";
 import { LinksCollection, Link } from "./links";
 
-const getLinks = async () => {
-  return await LinksCollection.find({});
-};
-
 Meteor.methods({
   async "links.getLinks"() {
     return LinksCollection.find({}).fetch();
